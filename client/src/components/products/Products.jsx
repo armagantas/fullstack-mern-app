5d362bec01dc90ea9500284b8e217502bc742f22
armagantas <65099385+armagantas@users.noreply.github.com>
2023-03-30 02:43:34 +0300

    commit

---
+++ client/src/components/products/Products.jsx
@@ -0,0 +1,78 @@
+const Products = () => {
+  return (
+    <div className="product-wrapper grid grid-cols-card gap-4">
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+            className="h-28 object-cover
+             w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Apple</span>
+          <span>12TL</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+            className="h-28 object-cover
+             w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Apple</span>
+          <span>12TL</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+            className="h-28 object-cover
+             w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Apple</span>
+          <span>12TL</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+            className="h-28 object-cover
+             w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Apple</span>
+          <span>12TL</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+            className="h-28 object-cover
+             w-full border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Apple</span>
+          <span>12TL</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
