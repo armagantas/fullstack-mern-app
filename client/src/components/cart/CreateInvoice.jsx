c9a5f3c9accedeb33875e20be8ca0d6429ca0ab3
armagantas <65099385+armagantas@users.noreply.github.com>
2023-03-31 03:22:08 +0300

    Commit

--- client/src/components/cart/CreateInvoice.jsx
+++ client/src/components/cart/CreateInvoice.jsx
@@ -1,7 +1,12 @@
-import { Modal } from "antd";
+import { Modal, Form, Input, Select, Card, Button } from "antd";
 
 const CreateInvoice = (props) => {
   const { isModalOpen, setIsModalOpen } = props;
+
+  const onFinish = (values) => {
+    console.log("Received values on form: ", values);
+  };
+
   return (
     <Modal
       title="Create Invoice"
@@ -9,9 +14,65 @@ const CreateInvoice = (props) => {
       footer={false}
       onCancel={() => setIsModalOpen(false)}
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <Form layout={"vertical"} onFinish={onFinish}>
+        <Form.Item
+          label="Customer Name/Surname"
+          name={"curtomerName"}
+          rules={[
+            {
+              requried: true,
+              message: "Please enter a valid name and surname!",
+            },
+          ]}
+        >
+          <Input placeholder="Enter a valid name and surname" />
+        </Form.Item>
+        <Form.Item
+          label="Phone Number"
+          name={"phoneNumber"}
+          rules={[{ requried: true }]}
+        >
+          <Input
+            placeholder="Please enter a valid phone number"
+            maxLength={11}
+          />
+        </Form.Item>
+        <Form.Item
+          label="Payment Type"
+          rules={[{ requried: true }]}
+          name={"paymentMode"}
+        >
+          <Select placeholder="Please choose a payment method">
+            <Select.Option value="Cash">Cash</Select.Option>
+            <Select.Option value="Credit Card">Credit Card</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Card>
+          <div className="flex justify-between">
+            <span>Subtotal</span>
+            <span>549.00</span>
+          </div>
+          <div className="flex justify-between my-2">
+            <span>Tax 8%</span>
+            <span className="text-red-600">43.92</span>
+          </div>
+          <div className="flex justify-between font-bold">
+            <span>Total</span>
+            <span>632.92</span>
+          </div>
+          <div className="flex justify-end">
+            <Button
+              className="mt-4"
+              type="primary"
+              onClick={() => setIsModalOpen(true)}
+              htmlType="submit"
+            >
+              Create Order
+            </Button>
+          </div>
+        </Card>
+      </Form>
     </Modal>
   );
 };
