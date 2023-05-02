d4a164b0e90eb403d8b8295a809bb7fb0daed1b3
armagantas <65099385+armagantas@users.noreply.github.com>
2023-05-02 17:24:00 +0300

    Commit

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,16 +1,26 @@
 const UserSchema = require("../models/user.js");
+const User = require("../models/user.js");
+const bcrypt = require("bcryptjs");
 
 const createUser = async (req, res) => {
   try {
-    const newUser = await UserSchema.create(req.body);
+    const { username, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
     res.status(201).json({
       newUser,
-      message: "User has been created.",
+      message: "User has been created",
       success: true,
     });
-  } catch (error) {
+  } catch (err) {
     return res.status(500).json({
-      message: error.message,
+      message: err.message,
     });
   }
 };
--- api/models/user.js
+++ api/models/user.js
@@ -14,10 +14,6 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    verify: {
-      type: Boolean,
-      required: true,
-    },
   },
   { timestamps: true }
 );
