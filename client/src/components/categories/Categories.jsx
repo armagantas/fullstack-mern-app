3c24442ee325d5c2cd94ede0d38e10486acef094
armagantas <65099385+armagantas@users.noreply.github.com>
2023-05-05 21:04:13 +0300

    Commit

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,27 +1,10 @@
 import { useState } from "react";
 import "./style.css";
 import { PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Modal } from "antd";
+import CreateCategoryModal from "../modals/CreateCategoryModal";
 
 const Categories = () => {
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-
-  const onFinish = async (values) => {
-    try {
-      const response = await fetch(
-        "http://localhost:5000/api/categories/createCategory",
-        {
-          method: "POST",
-          headers: { "Content-type": "application / json;" },
-          body: JSON.stringify(values),
-        }
-      );
-      const result = await response.json();
-      console.log("Success:", result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <ul className="flex gap-4 md:flex-col text-lg ">
@@ -55,27 +38,10 @@ const Categories = () => {
       >
         <PlusOutlined className="md:text-2xl" />
       </li>
-      <Modal
-        title="Add a new category"
+      <CreateCategoryModal
         open={isAddModalOpen}
         onCancel={() => setIsAddModalOpen(false)}
-        footer={false}
-      >
-        <Form layout="vertical" onFinish={onFinish}>
-          <Form.Item
-            name="title"
-            label="Add Category"
-            rules={[{ required: true, message: "Category name is required" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item className="flex justify-end mb-0">
-            <Button type="primary" htmlType="submit">
-              Add
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
+      />
     </ul>
   );
 };
---
+++ client/src/components/modals/CreateCategoryModal.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import axios from "axios";
+
+import { Button, Form, Input, Modal } from "antd";
+import { useForm } from "antd/es/form/Form";
+
+const CreateCategoryModal = ({ ...other }) => {
+  const [form] = useForm();
+  const onFinish = async (values) => {
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}/categories/createCategory`,
+        {
+          ...values,
+        }
+      );
+      form.resetFields();
+      other.onCancel();
+    } catch (err) {
+      console.log(err);
+      other.onCancel();
+    }
+  };
+
+  return (
+    <Modal title="Add a new category" footer={false} {...other}>
+      <Form layout="vertical" onFinish={onFinish} form={form}>
+        <Form.Item
+          name="title"
+          label="Add Category"
+          rules={[{ required: true, message: "Category name is required" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item className="flex justify-end mb-0">
+          <Button type="primary" htmlType="submit">
+            Add
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default CreateCategoryModal;
