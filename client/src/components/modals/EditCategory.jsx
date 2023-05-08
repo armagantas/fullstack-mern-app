58e1254f11a45c41f0f1eca6ea9cd933a0c9f2c2
armagantas <65099385+armagantas@users.noreply.github.com>
2023-05-09 02:48:38 +0300

    Commit

--- client/src/components/modals/EditCategory.jsx
+++ client/src/components/modals/EditCategory.jsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Modal, Table, Input, Button } from "antd";
+import axios from "axios";
 
 const EditCategory = ({ isEditModalOpen, setIsEditModalOpen, categories }) => {
+  const [editingRow, setEditingRow] = useState({});
+
+  console.log(editingRow._id);
+
+  const onFinish = (values) => {
+    try {
+      axios.put(`${process.env.REACT_APP_API_URL}/categories/updateCategory`, {
+        ...values,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const columns = [
     {
       title: "Category Title",
       dataIndex: "title",
-      render: () => {
-        return (
-          <Form.Item className="mb-0">
-            <Input />
-          </Form.Item>
-        );
+      render: (_, record) => {
+        if (record._id === editingRow._id) {
+          return (
+            <Form.Item className="mb-0">
+              <Input defaultValue={record.title} />
+            </Form.Item>
+          );
+        } else {
+          return <p>{record.title}</p>;
+        }
       },
     },
     {
       title: "Action",
       dataIndex: "action",
-      render: () => {
+      render: (text, record) => {
         return (
           <>
-            <Button type="link">Edit</Button>
-            <Button type="text">Save</Button>
+            <Button type="link" onClick={() => setEditingRow(record)}>
+              Edit
+            </Button>
+            <Button type="text" htmlType="submit">
+              Save
+            </Button>
             <Button type="text" danger>
               Delete
             </Button>
@@ -38,8 +60,13 @@ const EditCategory = ({ isEditModalOpen, setIsEditModalOpen, categories }) => {
       footer={false}
       onCancel={() => setIsEditModalOpen(false)}
     >
-      <Form>
-        <Table bordered dataSource={categories} columns={columns} />
+      <Form onFinish={onFinish}>
+        <Table
+          bordered
+          dataSource={categories}
+          columns={columns}
+          rowKey={"_id"}
+        />
       </Form>
     </Modal>
   );
